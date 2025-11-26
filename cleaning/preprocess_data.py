246dd3dd286fb1ffef284aad9877e551aaee1797
Amirali Iranmanesh <34022796+aircode610@users.noreply.github.com>
2025-11-26 02:22:35 +0100

    Added hapax ratio filterpython

--- cleaning/preprocess_data.py
+++ cleaning/preprocess_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 from remove_boilerplates import remove_html_tags, remove_boilerplate_patterns
-from lexical_quality_check import filter_by_ttr, filter_by_entropy, filter_by_mtld, filter_by_hapax_ratio
+from lexical_quality_check import filter_by_ttr, filter_by_entropy, filter_by_mtld, filter_by_hapax_ratio, filter_by_repetition_ratio
 
 data_path = os.path.join('data', 'Final_table_results.xlsx')
 
@@ -20,6 +20,7 @@ print(f"Sampled {len(df_sample)} rows")
 # df_cleaned = filter_by_entropy(df_cleaned, column_name='content_text', entropy_threshold=4.0, drop_below=True)
 # df_cleaned = filter_by_mtld(df_cleaned, column_name='content_text', mtld_threshold=9.0, ttr_threshold=0.90, min_words=5)
 # df_cleaned = filter_by_hapax_ratio(df_cleaned, column_name='content_text', hapax_threshold=0.3, drop_below=True)
+df_cleaned = filter_by_repetition_ratio(df_cleaned, column_name='content_text', repetition_threshold=0.3, drop_above=True)
 
 print(f"\nData preprocessing complete!")
 print(f"Final DataFrame shape: {df_cleaned.shape}")
